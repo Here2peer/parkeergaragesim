cfd39c3b4f115942e00f6064b4dda766f9cddcb0
here2peer <timothyravor@live.nl>
2017-02-07 18:39:36 +0100

    Added start of dynamicModelThread

---
+++ src/model/MVCDynamicModelThread.java
@@ -0,0 +1,18 @@
+package model;
+
+import javax.swing.*;
+
+/**
+ * Created by timothy on 7-2-17.
+ */
+
+public class MVCDynamicModelThread {
+    private Model model;
+    private JFrame screen;
+    private View countview;
+    private Controller controller;
+
+    public MVCDynamicModelThread() {
+
+    }
+}
