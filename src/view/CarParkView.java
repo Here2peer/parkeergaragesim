ccfd3f6b1409dee783045b1c4aa913a6724ea27b
here2peer <timothyravor@live.nl>
2017-02-13 14:35:48 +0100

    Give birth to CarParkView

---
+++ src/view/CarParkView.java
@@ -0,0 +1,102 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+import model.Car;
+import model.Location;
+
+/**
+ * Created by timothy on 13-2-17.
+ */
+public class CarParkView extends JPanel {
+
+    private Dimension size;
+    private Image carParkImage;
+    private SimulatorView simulatorView;
+
+    /**
+     * Constructor for objects of class CarPark
+     */
+    public CarParkView(SimulatorView simulatorView) {
+        this.simulatorView = simulatorView;
+        size = new Dimension(0, 0);
+    }
+
+    /**
+     * Overridden. Tell the GUI manager how big we would like to be.
+     */
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 500);
+    }
+
+    /**
+     * Overriden. The car park view component needs to be redisplayed. Copy the
+     * internal image to screen.
+     */
+    public void paintComponent(Graphics g) {
+        if (carParkImage == null) {
+            return;
+        }
+
+        Dimension currentSize = getSize();
+        if (size.equals(currentSize)) {
+            g.drawImage(carParkImage, 0, 0, null);
+        }
+        else {
+            // Rescale the previous image.
+            g.drawImage(carParkImage, 0, 0, currentSize.width, currentSize.height, null);
+        }
+    }
+
+    /**
+     * Create a new car park image if the size has changed.
+     * Goes through all parking spot and draws it according to if it is occupied, and if so, according to the type of the occupying car.
+     */
+    public void updateView() {
+        if (!size.equals(getSize())) {
+            size = getSize();
+            carParkImage = createImage(size.width, size.height);
+        }
+        Graphics graphics = carParkImage.getGraphics();
+        for(int floor = 0; floor < simulatorView.getNumberOfFloors()-1; floor++) {
+            for(int row = 0; row < simulatorView.getNumberOfRows(); row++) {
+                for(int place = 0; place < simulatorView.getNumberOfPlaces(); place++) {
+                    Location location = new Location(floor, row, place);
+                    Car car = simulatorView.getCarAt(location);
+                    Color color = car == null ? Color.decode("#E2E2E2") : car.getColor();
+                    drawPlace(graphics, location, color);
+                }
+            }
+        }
+
+        // Hardcoded, could be done differently
+        for(int floor = 2; floor < simulatorView.getNumberOfFloors(); floor++) {
+            for(int row = 0; row < simulatorView.getNumberOfRows(); row++) {
+                for(int place = 0; place < simulatorView.getNumberOfPlaces(); place++) {
+                    Location location = new Location(floor, row, place);
+                    Car car = simulatorView.getCarAt(location);
+                    Color color = car == null ? Color.decode("#C71585") : car.getColor();
+                    drawPlace(graphics, location, color);
+                }
+            }
+        }
+        repaint();
+    }
+
+    /**
+     * Paint a place on this car park view in a given color.
+     */
+    private void drawPlace(Graphics graphics, Location location, Color color) {
+        graphics.setColor(color);
+        graphics.fillRect(
+                location.getFloor() * 260 + (1 + (int)Math.floor(location.getRow() * 0.5)) * 75 + (location.getRow() % 2) * 20,
+                60 + location.getPlace() * 10,
+                20 - 1,
+                10 - 1); // TODO use dynamic size or constants
+    }
+}
--- src/view/SimulatorView.java
+++ src/view/SimulatorView.java
@@ -31,7 +31,7 @@ public class SimulatorView extends AbstractView {
 
         cars = new Car[numberOfFloors][numberOfRows][numberOfPlaces];
 
-        carParkView = new CarParkView();
+        carParkView = new CarParkView(this);
 
         Container contentPane = getContentPane();
         contentPane.add(carParkView, BorderLayout.CENTER);
@@ -232,92 +232,6 @@ public class SimulatorView extends AbstractView {
             return false;
         }
         return true;
-    }
-
-    private class CarParkView extends JPanel {
-
-        private Dimension size;
-        private Image carParkImage;
-
-        /**
-         * Constructor for objects of class CarPark
-         */
-        public CarParkView() {
-            size = new Dimension(0, 0);
-        }
-
-        /**
-         * Overridden. Tell the GUI manager how big we would like to be.
-         */
-        public Dimension getPreferredSize() {
-            return new Dimension(800, 500);
-        }
-
-        /**
-         * Overriden. The car park view component needs to be redisplayed. Copy the
-         * internal image to screen.
-         */
-        public void paintComponent(Graphics g) {
-            if (carParkImage == null) {
-                return;
-            }
-
-            Dimension currentSize = getSize();
-            if (size.equals(currentSize)) {
-                g.drawImage(carParkImage, 0, 0, null);
-            }
-            else {
-                // Rescale the previous image.
-                g.drawImage(carParkImage, 0, 0, currentSize.width, currentSize.height, null);
-            }
-        }
-
-        /**
-         * Create a new car park image if the size has changed.
-         * Goes through all parking spot and draws it according to if it is occupied, and if so, according to the type of the occupying car.
-         */
-        public void updateView() {
-            if (!size.equals(getSize())) {
-                size = getSize();
-                carParkImage = createImage(size.width, size.height);
-            }
-            Graphics graphics = carParkImage.getGraphics();
-            for(int floor = 0; floor < getNumberOfFloors()-1; floor++) {
-                for(int row = 0; row < getNumberOfRows(); row++) {
-                    for(int place = 0; place < getNumberOfPlaces(); place++) {
-                        Location location = new Location(floor, row, place);
-                        Car car = getCarAt(location);
-                        Color color = car == null ? Color.decode("#E2E2E2") : car.getColor();
-                        drawPlace(graphics, location, color);
-                    }
-                }
-            }
-
-            // Hardcoded, could be done differently
-            for(int floor = 2; floor < getNumberOfFloors(); floor++) {
-                for(int row = 0; row < getNumberOfRows(); row++) {
-                    for(int place = 0; place < getNumberOfPlaces(); place++) {
-                        Location location = new Location(floor, row, place);
-                        Car car = getCarAt(location);
-                        Color color = car == null ? Color.decode("#C71585") : car.getColor();
-                        drawPlace(graphics, location, color);
-                    }
-                }
-            }
-            repaint();
-        }
-
-        /**
-         * Paint a place on this car park view in a given color.
-         */
-        private void drawPlace(Graphics graphics, Location location, Color color) {
-            graphics.setColor(color);
-            graphics.fillRect(
-                    location.getFloor() * 260 + (1 + (int)Math.floor(location.getRow() * 0.5)) * 75 + (location.getRow() % 2) * 20,
-                    60 + location.getPlace() * 10,
-                    20 - 1,
-                    10 - 1); // TODO use dynamic size or constants
-        }
     }
 
 }
