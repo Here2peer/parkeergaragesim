425eb47891a1763962ea11e69b5dff3dc0890a2d
here2peer <timothyravor@live.nl>
2017-02-14 11:34:20 +0100

    Moved the model.run() to the main class CarParkMain

--- src/main/CarParkMain.java
+++ src/main/CarParkMain.java
@@ -1,8 +1,17 @@
 package main;
 
+import model.Model;
+import view.SimulatorView;
+
 /**
  * Created by Timothy on 6-2-2017.
  */
 
 public class CarParkMain {
+    public CarParkMain() {
+        Model model = new Model(3, 6, 30);
+        SimulatorView observer = new SimulatorView(model);
+        model.setObserver(observer);
+        model.run();
+    }
 }
--- src/run/run.java
+++ src/run/run.java
@@ -4,15 +4,12 @@ package run;
  * Created by timothy on 7-2-17.
  */
 
+import main.CarParkMain;
 import model.Model;
 import view.SimulatorView;
 
 public class run {
         public static void main(String[] args) {
-
-            Model model = new Model(3, 6, 30);
-            SimulatorView observer = new SimulatorView(model);
-            model.setObserver(observer);
-            model.run();
+            new CarParkMain();
         }
 }
