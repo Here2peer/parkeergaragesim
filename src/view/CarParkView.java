e79147b6dec6bcaa6bbd1764d835528541f6bda4
Somentus <somentus@gmail.com>
2017-02-16 06:19:40 +0100

    Fix location of stats and slow down speed of simulation to make information easier to read.

--- src/view/CarParkView.java
+++ src/view/CarParkView.java
@@ -110,29 +110,29 @@ public class CarParkView extends AbstractView {
         graphics.drawString("Current date: ", a, c);
         graphics.drawString(hourz + ":" + minutez + ", day " + days, b, c);
 
-        graphics.drawString("Total spots: ", a, c + 15);
-        graphics.drawString(String.valueOf(model.getNumberOfTotalPlaces()), b, c + 15);
+        graphics.drawString("Total revenue: ", a, c + 15);
+        graphics.drawString("€" + String.valueOf((int)model.getRoundTurnover()), b, c + 15);
 
-        graphics.drawString("Free spots: ", a, c  + 30);
-        graphics.drawString(String.valueOf(model.getNumberOfFreePlaces()), b, c + 30);
+        graphics.drawString("Total spots: ", a, c + 45);
+        graphics.drawString(String.valueOf(model.getNumberOfTotalPlaces()), b, c + 45);
 
-        graphics.drawString("Occupied spots: ", a, c + 45);
-        graphics.drawString(String.valueOf(model.getNumberOfOccupiedPlaces()), b, c + 45);
+        graphics.drawString("Free spots: ", a, c  + 60);
+        graphics.drawString(String.valueOf(model.getNumberOfFreePlaces()), b, c + 60);
 
-        graphics.drawString("Occupation rate: ", a, c + 60);
-        graphics.drawString(String.valueOf(model.getOccupationRate()) + "%", b, c + 60);
+        graphics.drawString("Occupied spots: ", a, c + 75);
+        graphics.drawString(String.valueOf(model.getNumberOfOccupiedPlaces()), b, c + 75);
 
-        graphics.drawString("Total revenue: ", a, c + 75);
-        graphics.drawString("€" + String.valueOf((int)model.getRoundTurnover()), b, c + 75);
+        graphics.drawString("Occupation rate: ", a, c + 90);
+        graphics.drawString(String.valueOf(model.getOccupationRate()) + "%", b, c + 90);
 
-        graphics.drawString("Normal cars: ", a, c + 90);
-        graphics.drawString(String.valueOf(model.getNumberOfNormalCars()), b, c + 90);
+        graphics.drawString("Normal cars: ", a, c + 120);
+        graphics.drawString(String.valueOf(model.getNumberOfNormalCars()), b, c + 120);
 
-        graphics.drawString("Reserved cars: ", a, c + 105);
-        graphics.drawString(String.valueOf(model.getNumberOfReservedCars()), b, c + 105);
+        graphics.drawString("Reserved cars: ", a, c + 135);
+        graphics.drawString(String.valueOf(model.getNumberOfReservedCars()), b, c + 135);
 
-        graphics.drawString("Pass holders: ", a, c + 120);
-        graphics.drawString(String.valueOf(model.getNumberOfParkingCars()), b, c + 120);
+        graphics.drawString("Pass holders: ", a, c + 150);
+        graphics.drawString(String.valueOf(model.getNumberOfParkingCars()), b, c + 150);
 
         repaint();
     }
